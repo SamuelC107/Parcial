5ecc295d9599035005ca853ac3d36ca12e4c4fb8
SamuelC107 <samuelcorrea1116986@correo.itm.edu.co>
2025-08-27 21:17:51 -0500

    variables main

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+public class Estados
+{
+    public int Id;
+    public string? Nombre;
+    public List<Personas>? Personas;
+}
+
+public class VideoJuegos
+{
+    public int Id;
+    public string? Nombre;
+}
+
+public class Personas
+{
+    public int Id;
+    public string? Nombre;
+    public decimal Estatura;
+    public bool Vive;
+    public DateTime Fecha;
+    public Estados? Estado;
+    public List<VideoJuegos>? VideoJuegos;
+}
